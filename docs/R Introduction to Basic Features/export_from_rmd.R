744c28635aa1399c6ddab9341ccac10ad15a36d5
Ben Wright <chthonic.games@gmail.com>
2022-09-27 10:32:13 +0100

    Fold into 1 document and update

--- docs/R Introduction to Basic Features/export_from_rmd.R
+++ docs/R Introduction to Basic Features/export_from_rmd.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
-rmarkdown::render("R introduction to Basic Features.rmd",
+rmarkdown::render("R-introduction-to-Basic-Features.rmd",
     output_format=c("md_document", "html_document", "pdf_document"))
