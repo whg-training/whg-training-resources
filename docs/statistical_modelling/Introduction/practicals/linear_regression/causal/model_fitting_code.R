e07d40ca2a06ced4a54905da04e1dd6dc81c9990
Gavin Band <gavin.band@well.ox.ac.uk>
2022-10-06 10:14:51 +0100

    Cleaned out stats modelling folder

--- docs/statistical_modelling/Introduction/practicals/linear_regression/causal/model_fitting_code.R
+++
@@ -1,8 +0,0 @@
-n = 7
-data = read.table( sprintf( "diagram%d_simulated_data.tsv", n ), hea=T, as.is=T, sep ="\t" )
-
-# Fit model without B
-l = lm( C ~ A, data = data ); summary(l)$coeff
-
-# Fit model with B
-l = lm( C ~ A + B, data = data ); summary(l)$coeff
