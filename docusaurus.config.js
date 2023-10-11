27d46763d6a3b2e1759fffcd56aa62260671f263
Gavin Band <gavin.band@well.ox.ac.uk>
2023-10-11 09:12:06 +0100

    Remove R support again as breaks live site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,8 +114,7 @@ const config = {
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: [ 'r' ]
+        darkTheme: darkCodeTheme
       },
     }),
 };
