0ef8a72bada6017bddc65161547fb8b664852e73
Gavin Band <gavin.band@well.ox.ac.uk>
2023-10-16 09:44:10 +0100

    Make gmsgff package in analysing tutorial

---
+++ docs/programming/analysing_genes_across_species/code/R/gmsgff/R/parse_gff3_to_dataframe.R
@@ -0,0 +1,48 @@
+echo <- function( message, ... ) {
+    cat( sprintf( message, ... ))
+}
+
+parse_gff3_to_dataframe <-
+function(
+    filename,
+    extra_attributes = c()
+) {
+    echo( "++parse_gff3_to_dataframe(): Reading GFF3 data from \"%s\"...\n", filename )
+    result = read_gff3( filename )
+
+    result = tibble::add_column( result, ID = NA, .before = 1 )
+    result = tibble::add_column( result, Parent = NA, .before = 2 )
+
+    for( attribute in c( "ID", "Parent", extra_attributes )) {
+        echo( "++parse_gff3_to_dataframe(): Extracting \"%s\" attribute...\n", attribute )
+        # Updated regex that also matches the trailing semicolon or end-of-string
+        # so we can remove it
+        regex = sprintf( "%s=([^;]+)[;|$]", attribute )
+        # Extract the matching part
+        result[[attribute]] = stringr::str_extract( result[['attributes']], regex, group = TRUE )
+        # Remove from the attributes
+        result[['attributes']] = stringr::str_remove( result[['attributes']], regex )
+    }
+    echo( "++parse_gff3_to_dataframe(): ok.\n" )
+    return( result )
+}
+
+read_gff3 <- function( filename ) {
+    readr::read_tsv(
+        filename,
+        comment = '#',
+        na = ".",
+        col_names = c( 'seqid', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes' ),
+        col_types = readr::cols(
+            readr::col_character(),
+            readr::col_character(),
+            readr::col_character(),
+            readr::col_integer(),
+            readr::col_double(),
+            readr::col_double(),
+            readr::col_character(),
+            readr::col_integer(),
+            readr::col_character()
+        )
+    )
+}
